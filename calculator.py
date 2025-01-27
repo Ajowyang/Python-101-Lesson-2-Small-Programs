da7b1532c3e9eb2e6f9e284b9226d05001ffd4c1
ajowyang <andrewowyang875@gmail.com>
2025-01-26 20:39:44 -0800

    refactored calculator

--- calculator.py
+++ calculator.py
@@ -6,18 +6,44 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
-print("Welcome to calculator!")
-num1 = float(input("What's the first number? "))
-num2 = float(input("What's the second number? "))
-operation = input('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide ')
+def prompt(message):
+	print(f'==> {message}')
 
+def invalid_num(num_str):
+	try:
+		int(num_str)
+	except ValueError:
+		return True
+	return False
+
+
+prompt("Welcome to calculator!")
+
+prompt("What's the first number? ")
+num1 = input()
+while invalid_num(num1):
+	prompt("Hmm.. doesn't look like valid number")
+	num1 = input()
+
+prompt("What's the second number? ")
+num2 = input()
+while invalid_num(num2):
+	prompt("Hmm.. doesn't look like valid number")
+	num2 = input()
+
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide ')
+operation = input()
+while operation not in ['1', '2', '3', '4']:
+	prompt("Must choose 1, 2, 3, or 4")
+	operation = input()
+	
 match operation:
 	case '1':
-		print(f"The result is: {num1 + num2}")
+		prompt(f"The result is: {int(num1) + int(num2)}")
 	case '2':
-		print(f"The result is: {num1 - num2}")
+		prompt(f"The result is: {int(num1) - int(num2)}")
 	case '3':
-		print(f"The result is: {num1 * num2}")
+		prompt(f"The result is: {int(num1) * int(num2)}")
 	case '4':
-		print(f"The result is: {num1 / num2}")
+		prompt(f"The result is: {int(num1) / int(num2)}")
 
