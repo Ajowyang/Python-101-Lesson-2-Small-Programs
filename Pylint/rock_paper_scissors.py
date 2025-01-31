2eaf7445da09ecdf6a3aefbfbef441d081767cef
ajowyang <andrewowyang875@gmail.com>
2025-01-30 23:19:18 -0800

    added spock and lizard to rock paper scissors

--- Pylint/rock_paper_scissors.py
+++ Pylint/rock_paper_scissors.py
@@ -1,42 +1,51 @@
 import random
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
-PLAYING_AGAIN = True
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+WINNING_COMBOS = {
+    'rock': ['scissors', 'lizard'],
+    'paper': ['rock', 'spock'],
+    'scissors': ['paper', 'lizard'],
+    'lizard': ['paper', 'spock'],
+    'spock': ['rock', 'scissors'],
+}
+PLAYING = True
 
 def prompt(message):
     print(f"==> {message}")
 
+def player_wins(player_choice, comp_choice):
+    return comp_choice in WINNING_COMBOS[player_choice]
+
 def display_winner(player_choice, comp_choice):
     prompt(f"You chose {player_choice}, computer chose {comp_choice}")
-    if ((player_choice == "rock" and comp_choice == "scissors") or
-        (player_choice == "paper" and comp_choice == "rock") or
-        (player_choice == "scissor" and comp_choice == "paper")):
+    if player_wins(player_choice, comp_choice):
         prompt("You win!")
-    elif ((player_choice == "rock" and comp_choice == "paper") or
-        (player_choice == "paper" and comp_choice == "scissors") or
-        (player_choice == "scissors" and comp_choice == "rock")):
-        prompt("Computer wins!")
-    else:
+    elif player_choice == comp_choice:
         prompt("It's a tie!")
+    else:
+        prompt("Computer wins!")
 
-while PLAYING_AGAIN:
+def prompt_and_validate_input():
     prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
-    choice = input()
-
+    player_choice = input()
     while choice not in VALID_CHOICES:
         prompt("That's not a valid choice")
-        choice = input()
+        player_choice = input()
+    return player_choice
 
+def prompt_play_again():
+    prompt("Do you want to play again? (y/n)")
+    y_or_n = input()
+    while y_or_n.lower() not in ['y', 'n']:
+        prompt("Enter a valid choice (y/n)")
+        y_or_n = input().lower()
+    return y_or_n
+
+while PLAYING:
+    choice = prompt_and_validate_input()
     computer_choice = random.choice(VALID_CHOICES)
-
     display_winner(choice, computer_choice)
 
-    prompt("Do you want to play again? (y/n)")
-    play_again = input()
-
-    while play_again.lower() not in ['y', 'n']:
-        prompt("Enter a valid choice (y/n)")
-        play_again = input().lower()
-
+    play_again = prompt_play_again()
     if play_again.lower() == 'n':
-        PLAYING_AGAIN = False
+        PLAYING = False
