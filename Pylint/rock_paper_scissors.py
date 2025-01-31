a8c98897d29dff2edf1fce89d534a5e22f39a608
ajowyang <andrewowyang875@gmail.com>
2025-01-31 00:47:32 -0800

    implemented all rps bonus features

--- Pylint/rock_paper_scissors.py
+++ Pylint/rock_paper_scissors.py
@@ -1,6 +1,14 @@
 import random
 
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+VALID_SHORTENED_INPUTS = ['r', 'p', 'sc', 'l', 'sp']
+SHORTENED_INPUT_CHOICES = {
+    'r': 'rock',
+    'p': 'paper',
+    'sc': 'scissors',
+    'l': 'lizard',
+    'sp': 'spock'
+}
 WINNING_COMBOS = {
     'rock': ['scissors', 'lizard'],
     'paper': ['rock', 'spock'],
@@ -8,13 +16,19 @@ WINNING_COMBOS = {
     'lizard': ['paper', 'spock'],
     'spock': ['rock', 'scissors'],
 }
-PLAYING = True
+WINS = {
+    'player': 0,
+    'computer': 0
+}
 
 def prompt(message):
     print(f"==> {message}")
 
 def player_wins(player_choice, comp_choice):
     return comp_choice in WINNING_COMBOS[player_choice]
+
+def computer_wins(player_choice, comp_choice):
+    return player_choice in WINNING_COMBOS[comp_choice]
 
 def display_winner(player_choice, comp_choice):
     prompt(f"You chose {player_choice}, computer chose {comp_choice}")
@@ -28,9 +42,13 @@ def display_winner(player_choice, comp_choice):
 def prompt_and_validate_input():
     prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
     player_choice = input()
+    if player_choice in VALID_SHORTENED_INPUTS:
+        player_choice = SHORTENED_INPUT_CHOICES[player_choice]
     while player_choice not in VALID_CHOICES:
         prompt("That's not a valid choice")
         player_choice = input()
+        if player_choice in VALID_SHORTENED_INPUTS:
+            player_choice = SHORTENED_INPUT_CHOICES[player_choice]
     return player_choice
 
 def prompt_play_again():
@@ -41,11 +59,31 @@ def prompt_play_again():
         y_or_n = input().lower()
     return y_or_n
 
-while PLAYING:
+def is_round_finished():
+    if WINS['player'] >= 3:
+        prompt("Player has won the round")
+        return True
+    if WINS['computer'] >= 3:
+        prompt("Computer has won the round")
+        return True
+    return False
+
+while True:
     choice = prompt_and_validate_input()
     computer_choice = random.choice(VALID_CHOICES)
+
+    if player_wins(choice, computer_choice):
+        WINS['player'] += 1
+    elif computer_wins(choice, computer_choice):
+        WINS['computer'] += 1
     display_winner(choice, computer_choice)
+    prompt(f"You have {WINS['player']} wins. "
+           f"Computer has {WINS['computer']} wins.")
+
+    ROUND_FINISHED = is_round_finished()
+    if ROUND_FINISHED:
+        break
 
     play_again = prompt_play_again()
     if play_again.lower() == 'n':
-        PLAYING = False
+        break
